4d99fff9251782f66fa0af36e69bf2d89e1dead5
Kiwi Wang <mm412537r@hotmail.com>
2015-01-20 18:01:53 +0800

    fixed path to halide_generated.h

--- jni/Android.mk
+++ jni/Android.mk
@@ -10,7 +10,7 @@ LOCAL_LDFLAGS   := -Ljni
 LOCAL_LDLIBS    := -lm -llog -landroid jni/halide_generated_/halide_generated.o # -lOpenCL -lllvm-a3xx
 LOCAL_STATIC_LIBRARIES := android_native_app_glue
 #LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../include $(LOCAL_PATH)/halide_generated_$(TARGET_ARCH_ABI)/
-LOCAL_C_INCLUDES := /home/kiwi/Documents/halide/include /home/kiwi/Documents/Android-Bigdata-files/final/HelloAndroid/jni/halide_generated_/
+LOCAL_C_INCLUDES := /home/kiwi/Documents/halide/include /home/kiwi/Documents/Android-Bigdata-files/final/HelloAndroid2/jni/halide_generated_/
 
 include $(BUILD_SHARED_LIBRARY)
 
